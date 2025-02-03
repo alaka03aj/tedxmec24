5c55552124250f5f5e89b6e461474182ea495057
Alaka A J <alaka03aj@gmail.com>
2025-02-03 23:23:05 +0530

    Added: Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,13 +2,13 @@ import MasonryGrid from '../components/MasonryGrid'
 const Gallery = () => {
   return (
     <div className="scroll-smooth overflow-hidden">
-      <div className="w-full md:pt-10 md:pb-20 px-4 md:px-16 justify-between">
-        <div className="flex flex-col gap-5 md:flex-row md:gap-10 md:pt-[7rem]">
-            <h1 className="">Gallery</h1>
-            <div>
-                <p>We have tried our best to capture the most amazing moments of TEDxMEC 2023 using each image &apos; s <i>dimensions</i>. Get it ?!</p>
+      <div className="w-full md:pt-10 md:pb-10 px-4 md:px-16 justify-between">
+        <div className=" text-secondary-white2 pt-[5rem]">
+            <h1 className="font-bold font-helvetica text-[36px] text-center text-transparent bg-clip-text bg-gradient-to-r from-white to-secondary-black2 sm:text-5xl">Gallery</h1>
+            {/* <div>
+                <p className='text-xl font-helvetica'>We have tried our best to capture the most amazing moments of TEDxMEC 2023 using each image &apos; s <i>dimensions</i>. Get it ?!</p>
                 <button>Dive into the dimensions</button>
-            </div>
+            </div> */}
         </div>
         <MasonryGrid />
       </div>
