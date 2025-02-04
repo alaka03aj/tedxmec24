f27b8cbc6af0275f5b77b8579beac56466b721e9
Alaka A J <alaka03aj@gmail.com>
2025-02-05 00:50:48 +0530

    Font: Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -4,7 +4,7 @@ const Gallery = () => {
     <div className="scroll-smooth overflow-hidden">
       <div className="w-full md:pt-10 md:pb-10 px-4 md:px-16 justify-between">
         <div className=" text-secondary-white2 pt-[5rem]">
-            <h1 className="font-bold font-helvetica text-[36px] text-center text-transparent bg-clip-text bg-gradient-to-r from-white to-secondary-black2 sm:text-5xl">Gallery</h1>
+            <h1 className="font-bold font-helvetica text-[36px] text-center sm:text-5xl">Gallery</h1>
             {/* <div>
                 <p className='text-xl font-helvetica'>We have tried our best to capture the most amazing moments of TEDxMEC 2023 using each image &apos; s <i>dimensions</i>. Get it ?!</p>
                 <button>Dive into the dimensions</button>
